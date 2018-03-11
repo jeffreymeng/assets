17555792955b05204fccbc12d3219213a5476572
Jeffrey Meng <jeffkmeng@gmail.com>
2018-03-11 11:22:58 -0700

    bug fi

---
+++ js/index.js
@@ -0,0 +1,49 @@
+var clipboard = new ClipboardJS(".link", {
+    target: function (trigger) {
+        //console.log(trigger);
+        return trigger;
+    }
+});
+$(".link").each(function () {
+    $(this).attr("data-toggle", "tooltip")
+    $(this).on("mouseleave", clearTooltip);
+    $(this).on("blur", clearTooltip);
+});
+
+
+function clearTooltip(e) {
+    e.currentTarget.setAttribute("class", "link");
+    e.currentTarget.removeAttribute("aria-label");
+}
+
+function showTooltip(elem, msg) {
+
+    elem.attr("data-title", msg);
+    elem.tooltip("show");
+}
+
+function fallbackMessage(action) {
+    var actionMsg = "";
+    var actionKey = (action === "cut" ? "X" : "C");
+
+    if (/iPhone|iPad/i.test(navigator.userAgent)) {
+        actionMsg = "No support :(";
+    }
+    else if (/Mac/i.test(navigator.userAgent)) {
+        actionMsg = "Press ⌘-" + actionKey + " to " + action;
+    }
+    else {
+        actionMsg = "Press Ctrl-" + actionKey + " to " + action;
+    }
+
+    return actionMsg;
+}
+clipboard.on("success", function (e) {
+    e.clearSelection();
+
+    showTooltip(e.trigger, "Copied!");
+});
+
+clipboard.on("error", function (e) {
+    showTooltip(e.trigger, fallbackMessage(e.action));
+});
